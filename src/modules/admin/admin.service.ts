18e0f7801e85648d6d0e1a0891b166a5af31a318
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-29 15:45:01 +0600

    implemented class base functionality

--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -1,29 +1,58 @@
 import AdminModel from "./admin.model";
 
+class AdminService {
+  private adminModel: typeof AdminModel;
+
+  constructor(adminModel: typeof AdminModel) {
+    this.adminModel = adminModel;
+  }
+
+  public async checkLoginStatusIntoDB() {
+    const result = await this.adminModel.find({ isLogin: true });
+    return result;
+  }
+
+  public async checkGetAccess(email: string, password: string) {
+    const isTrue = await this.adminModel.findOne({
+      $and: [{ email: email }, { password: password }],
+    });
+    return isTrue;
+  }
+
+  public async getLoginIntoDB() {
+    const result = await this.adminModel.updateOne(
+      { key: "password" },
+      { $set: { isLogin: true } }
+    );
+    return result;
+  }
+
+  public async getLogOutFromDB() {
+    const result = await this.adminModel.updateOne(
+      { key: "password" },
+      { $set: { isLogin: false } }
+    );
+    return result;
+  }
+}
+const adminServiceInstance = new AdminService(AdminModel);
+
+// Existing function calls using the instance of AdminService class
+
 const checkLoginStatusIntoDB = async () => {
-  const result = await AdminModel.find({ isLogin: true });
-  return result;
+  return adminServiceInstance.checkLoginStatusIntoDB();
 };
 
 const checkGetAccess = async (email: string, password: string) => {
-  const isTrue = await AdminModel.findOne({
-    $and: [{ email: email }, { password: password }],
-  });
-  return isTrue;
+  return adminServiceInstance.checkGetAccess(email, password);
 };
+
 const getLoginIntoDB = async () => {
-  const result = AdminModel.updateOne(
-    { key: "password" },
-    { $set: { isLogin: true } }
-  );
-  return result;
+  return adminServiceInstance.getLoginIntoDB();
 };
+
 const getLogOutFromDB = async () => {
-  const result = AdminModel.updateOne(
-    { key: "password" },
-    { $set: { isLogin: false } }
-  );
-  return result;
+  return adminServiceInstance.getLogOutFromDB();
 };
 
 export const AdminServices = {
